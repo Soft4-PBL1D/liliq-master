4082fdc182cd22b3b51586b415572ef78d11d13a
Vagrant Default User <vagrant@debian-jessie.vagrantup.com>
2016-06-24 11:05:48 +0900

    attend Change

--- web/students/detail.php
+++ web/students/detail.php
@@ -1,3 +1,8 @@
+<html lang="en">
+<head>
+<title></title>
+</head>
+<body>
 <?php
 error_reporting(0);
 session_start();
@@ -6,24 +11,60 @@ require("/var/www/Function/ClassAttendFunction/ClassAttendDB.php");
 $ClassAttendDB = new ClassAttendDB();
 $ClassAttendDB->classattend($_SESSION["USERID"],$_GET["date"]);
 if(!isset($ClassAttendDB->myname)){
-echo "休校日";
+  if($_GET["submit"]){
+    echo "申請しました";
+    echo "<a href=detail.php>modoru</a>";
+  }
+  else
+    echo "休校日";
 exit;
 }
-echo $ClassAttendDB->myname."さん<br>";
+?>
+<form method="get" action="">
+<?php echo $ClassAttendDB->myname."さん<br>";
 $genre=["出席","遅刻","欠席","就活","病欠","公欠","遅延認証待ち","就活認証待ち","登下校未処理"];
 echo $_GET["date"]."の出席状況<br>";
 for($i=1;$i<=5;$i++){
   if($ClassAttendDB->myattend[$i]!=null){
 echo "{$i}時限：".$genre[$ClassAttendDB->myattend[$i]]."\n";
-echo "<input type='radio' name='type' value=0>出席";
-echo "<input type='radio' name='type' value=1>遅刻";
-echo "<input type='radio' name='type' value=2>欠席";
-echo "<input type='radio' name='type' value=3>終活";
-echo "<input type='radio' name='type' value=4>病欠";
-echo "<input type='radio' name='type' value=5>高血";
+
+echo "<input type='radio' name='type$i' value=0";
+if($ClassAttendDB->myattend[$i]==0)
+echo " checked>出席";
+else
+echo ">出席";
+echo "<input type='radio' name='type$i' value=1";
+if($ClassAttendDB->myattend[$i]==1)
+echo " checked>遅刻";
+else
+echo ">遅刻";
+echo "<input type='radio' name='type$i' value=2";
+if($ClassAttendDB->myattend[$i]==2)
+echo " checked>欠席";
+else
+echo ">欠席";
+echo "<input type='radio' name='type$i' value=3";
+if($ClassAttendDB->myattend[$i]==3)
+echo " checked>就活";
+else
+echo ">就活";
+echo "<input type='radio' name='type$i' value=4";
+if($ClassAttendDB->myattend[$i]==4)
+echo " checked>病欠";
+else
+echo ">病欠";
+echo "<input type='radio' name='type$i' value=5";
+if($ClassAttendDB->myattend[$i]==5)
+echo " checked>公欠";
+else
+echo ">公欠";
 echo "<br>";
 }
 else
 echo "{$i}時限：おやすみ<br>";
 }
+echo "<input type='submit' name='submit' value='変更依頼'>";
+echo "</form>";
 ?>
+</body>
+  </html>
--- web/teacher/detail.php
+++ web/teacher/detail.php
@@ -1,3 +1,9 @@
+
+<html lang="en">
+<head>
+<title></title>
+</head>
+<body>
 <?php
 error_reporting(0);
 session_start();
@@ -11,24 +17,67 @@ $ClassAttendDB->classattend($_GET["id"],$_GET["date"]);
 else
 $ClassAttendDB->classattend($_SESSION["USERID"],$_GET["date"]);
 if(!isset($ClassAttendDB->myname)){
-echo "休校日";
+  if($_GET["submit"]){
+    if(isset($_GET["type1"]))$ClassAttendDB->classchange($_GET["myname"],$_GET["date"],$_GET["type1"],1);
+    if(isset($_GET["type2"]))$ClassAttendDB->classchange($_GET["myname"],$_GET["date"],$_GET["type2"],2);
+    if(isset($_GET["type3"]))$ClassAttendDB->classchange($_GET["myname"],$_GET["date"],$_GET["type3"],3);
+    if(isset($_GET["type4"]))$ClassAttendDB->classchange($_GET["myname"],$_GET["date"],$_GET["type4"],4);
+    if(isset($_GET["type5"]))$ClassAttendDB->classchange($_GET["myname"],$_GET["date"],$_GET["type5"],5);
+
+    echo "変更しました";
+  }
+  else
+    echo "休校日";
 exit;
 }
-echo $ClassAttendDB->myname."さん<br>";
+?>
+<form method="get" action="">
+<?php echo $ClassAttendDB->myname."さん<br>";
 $genre=["出席","遅刻","欠席","就活","病欠","公欠","遅延認証待ち","就活認証待ち","登下校未処理"];
 echo $_GET["date"]."の出席状況<br>";
 for($i=1;$i<=5;$i++){
   if($ClassAttendDB->myattend[$i]!=null){
 echo "{$i}時限：".$genre[$ClassAttendDB->myattend[$i]]."\n";
-echo "<input type='radio' name='type' value=0>出席";
-echo "<input type='radio' name='type' value=1>遅刻";
-echo "<input type='radio' name='type' value=2>欠席";
-echo "<input type='radio' name='type' value=3>終活";
-echo "<input type='radio' name='type' value=4>病欠";
-echo "<input type='radio' name='type' value=5>高血";
+
+echo "<input type='radio' name='type$i' value=0";
+if($ClassAttendDB->myattend[$i]==0)
+echo " checked>出席";
+else
+echo ">出席";
+echo "<input type='radio' name='type$i' value=1";
+if($ClassAttendDB->myattend[$i]==1)
+echo " checked>遅刻";
+else
+echo ">遅刻";
+echo "<input type='radio' name='type$i' value=2";
+if($ClassAttendDB->myattend[$i]==2)
+echo " checked>欠席";
+else
+echo ">欠席";
+echo "<input type='radio' name='type$i' value=3";
+if($ClassAttendDB->myattend[$i]==3)
+echo " checked>就活";
+else
+echo ">就活";
+echo "<input type='radio' name='type$i' value=4";
+if($ClassAttendDB->myattend[$i]==4)
+echo " checked>病欠";
+else
+echo ">病欠";
+echo "<input type='radio' name='type$i' value=5";
+if($ClassAttendDB->myattend[$i]==5)
+echo " checked>公欠";
+else
+echo ">公欠";
 echo "<br>";
 }
 else
-echo "{$i}時限：おやすみ<br>";
+echo "{$i}時限：おやすみff<br>";
 }
+echo "<input type='hidden' name='myname' value={$ClassAttendDB->myid}>";
+echo "<input type='hidden' name='date' value={$_GET["date"]}>";
+echo "<input type='submit' name='submit' value='変更'>";
+echo "</form>";
 ?>
+</body>
+  </html>
